dcbfd50d849a35c6c9f37498310799c428661659
yojisekiya <yojisekiya@gmail.com>
2022-04-24 10:36:21 +0900

    本番環境で Gmail を使用できるように設定

---
+++ config/initializers/mail.rb
@@ -0,0 +1,16 @@
+if Rails.env.production?
+  # 各自のドメインに置き換えること
+  host = "https://radiant-fortress-98587.herokuapp.com"
+  # メール配信に失敗した場合にエラーを発生
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    port: 587,
+    address: "smtp.gmail.com",
+    user_name: Rails.application.credentials.gmail[:address],
+    password: Rails.application.credentials.gmail[:password],
+    domain: host,
+    authentication: "plain",
+  }
+end
